261e956827f69817807f44be24d7144536e7c97c
Ahmed Hashim <devahmedswe@gmail.com>
2025-11-17 00:19:42 +0200

    integrate with docker

--- project/settings.py
+++ project/settings.py
@@ -2,6 +2,7 @@
 Django settings for project project. using Django 5.2.8.
 """
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,18 +25,18 @@ INSTALLED_APPS = [
     'orders',
 ]
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'PStaskDB',
-        'USER': 'PStaskUSER',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv("DB_NAME", "PStaskDB"),
+        'USER': os.getenv("DB_USER", "PStaskUSER"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "123456"),
+        'HOST': os.getenv("DB_HOST", "db"),   
+        'PORT': os.getenv("DB_PORT", "3306"),
         'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
